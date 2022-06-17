1a8b58fa21f67802cb50f6812d2e9b3b9657e417
Kirill Grunin <jettyk@gmail.com>
2022-06-17 14:12:42 +0500

    Initial commit

---
+++ WeekdayFinder/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  WeekdayFinder
+//
+//  Created by Kirill Grunin on 17.06.2022.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
---
+++ WeekdayFinder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeekdayFinder
+//
+//  Created by Kirill Grunin on 17.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
